5f95d71888f25eccdaf5995adb3c91cb726d8d6e
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-05 00:00:30 +0800

    Add files via upload

---
+++ commands/cat.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.export.run = async (bot,message,args) => {
+
+let {body} = await superagent
+.get(`https://random.cat/meow`);
+
+let catembed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setTitle("Cat :cat: ")
+.setImage(body.file);
+
+message.channel.send(catembed)
+
+}
+
+module.exports.help = {
+  name: "cat"
+
+}
